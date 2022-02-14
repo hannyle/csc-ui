17b61001f9e9d08a16abbe93eb1466f73bc810c4
Oskari Väistö <oskari.vaisto@csc.fi>
2022-02-14 11:36:36 +0100

    Revert "wat"
    This reverts commit e30ee79e65feffba62863b5d876c6e452f89e52f.

--- playground/angular/src/app/examples/c-button/c-button.component.ts
+++ playground/angular/src/app/examples/c-button/c-button.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Example } from '../example/Example';
-import * as template from './c-button.component.html';
 
 @Component({
   selector: 'app-c-button',
   templateUrl: './c-button.component.html',
-  styleUrls: ['./c-button.component.scss'],
-  providers: [{ provide: Example, useExisting: CButtonComponent }],
+  styleUrls: ['./c-button.component.scss']
 })
-export class CButtonComponent implements OnInit, Example {
-  templateText = template.default;
+export class CButtonComponent implements OnInit {
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
+
 }
