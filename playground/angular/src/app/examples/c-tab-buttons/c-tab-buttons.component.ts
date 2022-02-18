e8123716fa4a08b9671c608775757c0d977252f9
Oskari Väistö <oskari.vaisto@csc.fi>
2022-02-18 07:36:40 +0100

    Clean up the tab buttons example

--- playground/angular/src/app/examples/c-tab-buttons/c-tab-buttons.component.ts
+++ playground/angular/src/app/examples/c-tab-buttons/c-tab-buttons.component.ts
@@ -6,36 +6,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./c-tab-buttons.component.scss'],
 })
 export class CTabButtonsComponent implements OnInit {
-  value: 1;
+  example = `<c-tab-buttons cControl [(ngModel)]="selectedTab">
+  <c-tab-button
+    *ngFor="let tab of tabs; let i = index"
+    icon="storage"
+    [label]="tab.label"
+    [value]="tab.value"
+  >
+    <i class="mdi mdi-server" slot="icon"></i>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    <span *ngIf="i % 2 === 0">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+  </c-tab-button>
+</c-tab-buttons>
+
+<p>Active tab: {{ selectedTab }}</p>
+  `;
+
+  script = `selectedTab: '1';
+
+tabs = [
+  {
+    label: 'One',
+    value: '1',
+  },
+  {
+    label: 'Two',
+    value: '2',
+  },
+  {
+    label: 'Three',
+    value: '3',
+  },
+  {
+    label: 'Four',
+    value: '4',
+  },
+  {
+    label: 'Five',
+    value: '5',
+  },
+  {
+    label: 'Six',
+    value: '6',
+  },
+];
+  `;
+
+  selectedTab: '1';
 
   tabs = [
     {
-      label: 'uksi',
+      label: 'One',
       value: '1',
     },
     {
-      label: 'kaksi',
+      label: 'Two',
       value: '2',
     },
     {
-      label: 'kolme',
+      label: 'Three',
       value: '3',
     },
     {
-      label: 'nelä',
+      label: 'Four',
       value: '4',
     },
     {
-      label: 'visi',
+      label: 'Five',
       value: '5',
     },
     {
-      label: 'kusi',
+      label: 'Six',
       value: '6',
     },
   ];
-
-  currentTab = '1';
 
   constructor() {}
 
