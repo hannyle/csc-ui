92263e3bf09143b38fdb9665433b6d06e3ca2103
Oskari Väistö <oskari.vaisto@csc.fi>
2022-03-16 19:01:26 +0200

    Fixed the ripple centering issue with elements not equal in height and width

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -13,17 +13,26 @@ export function createRipple(
 
   rippleElement.classList.remove('animate');
 
-  const d = Math.max(element.offsetWidth, element.offsetHeight);
-
-  rippleElement.style.width = d + 'px';
-  rippleElement.style.height = d + 'px';
-
+  const width = element.offsetWidth;
+  const height = element.offsetHeight;
   const rect = (event.target as HTMLElement).getBoundingClientRect();
+
+  const d = Math.max(element.offsetWidth, element.offsetHeight);
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
 
-  rippleElement.style.top = center ? '0' : y - d / 2 + 'px';
-  rippleElement.style.left = center ? '0' : x - d / 2 + 'px';
+  const top = y - d / 2 + 'px';
+  const left = x - d / 2 + 'px';
+
+  const centerPosition = {
+    top: d === height ? '0px' : ((d - height) / 2) * -1 + 'px',
+    left: d === width ? '0px' : ((d - width) / 2) * -1 + 'px',
+  };
+
+  rippleElement.style.width = d + 'px';
+  rippleElement.style.height = d + 'px';
+  rippleElement.style.top = center ? centerPosition.top : top;
+  rippleElement.style.left = center ? centerPosition.left : left;
 
   rippleElement.classList.add('animate');
 
