a2afa536dbc154575355ad20e1be3800b230a3e7
Oskari Väistö <oskari.vaisto@csc.fi>
2022-03-31 08:50:03 +0300

    Add Vue playground

---
+++ playground/vue/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import CSCUiComponents from './components/CSCUiComponents.vue';
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <CSCUiComponents />
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+
+  * {
+    box-sizing: border-box;
+  }
+}
+</style>
