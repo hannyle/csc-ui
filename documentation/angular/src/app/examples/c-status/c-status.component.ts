b70c196e6b1e2ae7f5fd40dc7596e625e5b58cae
Oskari Väistö <oskari.vaisto@csc.fi>
2022-05-11 10:11:44 +0300

    Add c-status component

---
+++ documentation/angular/src/app/examples/c-status/c-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c-status',
+  templateUrl: './c-status.component.html',
+  styleUrls: ['./c-status.component.scss']
+})
+export class CStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
